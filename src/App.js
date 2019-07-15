9b9190cc43e61f4451342fe2430058ec970dc36d
Matthieu Faugere <matthieu.faugere@lifen.fr>
2019-07-15 16:57:56 +0200

    Read mail

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {MailParser} from 'mailparser'
+import FileReaderStream from 'filereader-stream'
 
 function App() {
+  const parser = new MailParser();
+  let attachments = [];
+
+  useEffect(() => {
+    axios
+    .get('mail.eml')
+    .then(({headers, data: emlData}) => {
+      console.log(emlData)
+      const type = headers['content-type']
+      const blob = new Blob([emlData], {type})
+      FileReaderStream(blob).pipe(parser)
+      parser.on('headers', headers => {
+          console.log(headers);
+      });
+    
+      parser.on('data', data => {
+          if (data.type === 'text') {
+              Object.keys(data).forEach(key => {
+                  console.log(key);
+                  console.log('----');
+                  console.log(data[key]);
+              });
+          }
+      
+          if (data.type === 'attachment') {
+              attachments.push(data);
+              data.chunks = [];
+              data.chunklen = 0;
+              let size = 0;
+              Object.keys(data).forEach(key => {
+                  if (typeof data[key] !== 'object' && typeof data[key] !== 'function') {
+                      console.log('%s: %s', key, JSON.stringify(data[key]));
+                  }
+              });
+              data.content.on('readable', () => {
+                  let chunk;
+                  while ((chunk = data.content.read()) !== null) {
+                      size += chunk.length;
+                      data.chunks.push(chunk);
+                      data.chunklen += chunk.length;
+                  }
+              });
+      
+              data.content.on('end', () => {
+                  data.buf = Buffer.concat(data.chunks, data.chunklen);
+                  console.log('%s: %s B', 'size', size);
+                  // attachment needs to be released before next chunk of
+                  // message data can be processed
+                  data.release();
+              });
+          }
+      });
+      
+      parser.on('end', () => {
+          console.log('READY');
+      
+          console.log(attachments);
+
+      });
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
